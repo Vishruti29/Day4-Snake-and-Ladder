2355c85fbcebf7b75987eafac2a137e39256e8e2
Vishruti Patil <vishruti2908@gmail.com>
2023-02-22 18:01:07 +0530

    initial position of player

---
+++ src/Day_4/Snake_and_Ladder.java
@@ -0,0 +1,9 @@
+package Day_4;
+
+public class Snake_and_Ladder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Program");
+        int playerPosition = 0;
+        System.out.println("Starting Position is " + playerPosition );
+    }
+}

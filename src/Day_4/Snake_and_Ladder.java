bdd9fb2e924cf9e8a9cc53301ed7732433fd40a1
Vishruti Patil <vishruti2908@gmail.com>
2023-02-22 23:47:28 +0530

    UC2 Player gets Random number from 1 to 6

--- src/Day_4/Snake_and_Ladder.java
+++ src/Day_4/Snake_and_Ladder.java
@@ -1,9 +1,12 @@
 package Day_4;
-
+import java.util.Random;
 public class Snake_and_Ladder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Program");
-        int playerPosition = 0;
+        int playerPosition = 0; // UC1 - initial position
         System.out.println("Starting Position is " + playerPosition );
+        Random random = new Random(); // UC2 - Random 1 to 6 roll die
+        int roll_die = random.nextInt(6+1);
+        System.out.println("Player gets number " + roll_die);
     }
 }

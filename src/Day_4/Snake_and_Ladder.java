ea7932f7fb27a3d76bf9677c174ddbdec178df61
Vishruti Patil <vishruti2908@gmail.com>
2023-02-23 00:50:58 +0530

    UC5 Player reaches exact winning position 100

--- src/Day_4/Snake_and_Ladder.java
+++ src/Day_4/Snake_and_Ladder.java
@@ -8,30 +8,33 @@ public class Snake_and_Ladder {
         String playerName = s.next();
         int playerPosition = 0; // UC1 - initial position
         System.out.println(" Player Name : " + playerName );
+
         while ( winningPosition > playerPosition )
         {
             int die = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-            System.out.println(" Die roll number : " + die);
+            int remainingPosition = winningPosition - playerPosition ;
             int option = (int) (Math.floor(Math.random() * 10) % 3);
-            switch (option) {
-                case 0:
-                    System.out.println("No Play");
-                    playerPosition += 0;
-                    break;
+            if ( remainingPosition >= die ) {
+                switch (option) {
+                    case 0:
+                        System.out.println("No Play");
+                        playerPosition += 0;
+                        break;
 
-                case 1:
-                    System.out.println("Ladder");
-                    playerPosition += die;
-                    if (playerPosition > 100)
-                        playerPosition = 100;
-                    break;
+                    case 1:
+                        System.out.println("Ladder");
+                        playerPosition += die;
+                        if (playerPosition > 100)
+                            playerPosition = 100;
+                        break;
 
-                default:
-                    System.out.println("Snake");
-                    playerPosition -= die;
-                    if (playerPosition < 0)
-                        playerPosition = 0;
-                    break;
+                    default:
+                        System.out.println("Snake");
+                        playerPosition -= die;
+                        if (playerPosition < 0)
+                            playerPosition = 0;
+                        break;
+                }
             }
         }
         System.out.println("Player Position : " + playerPosition);
